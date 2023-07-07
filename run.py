99e76619850044f2c044312500bb2b47234528e2
dafedin <tm.i.sm.master@gmail.com>
2023-07-07 21:18:27 +0300

    Logger conflict removed
    logger.py and all dependancies removed.

--- run.py
+++ run.py
@@ -1,4 +1,43 @@
-from app import main
+import argparse
+from faker import Faker
+
+
+fake = Faker()
+
+fake_name = fake.name()
+
+
+def print_fake_name() -> None:
+    print(f'My name is {fake_name}.')
+
+
+parser = argparse.ArgumentParser(description='My first and simple CLI program.')
+
+parser.add_argument('--name', help='Your name.')
+
+args = parser.parse_args()
+
+
+def is_name() -> bool:
+    if args.name is None:
+        return False
+    else:
+        return True
+
+
+def parse_name() -> None:
+    if args.name:
+        print(f'Hello, {args.name}!')
+
+
+def main() -> None:
+    if is_name():
+        parse_name()
+        print_fake_name()
+    else:
+        print("Tell me Your name, please. (Run cmd [--help] for help)")
+        msg = 'Failed'
+
 
 if __name__ == '__main__':
     main()
